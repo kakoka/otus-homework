4e0163889d851c08929720538d08f1e0d8652f38
Pavel Ko <kakoka@gmail.com>
2018-11-06 10:46:35 +0300

    Update resizerootxfs-local.sh

--- hw03/dracut/module.d/91resizerootxfs/resizerootxfs-local.sh
+++ hw03/dracut/module.d/91resizerootxfs/resizerootxfs-local.sh
@@ -11,5 +11,11 @@ umount /tmp
 umount /mnt
 lvm lvreduce --config 'global {locking_type=1}' --yes -f -L -12G /dev/vg0/lvm_root
 mkfs.xfs -f -m uuid=1b615c91-2ba1-4877-a06d-fa5d14844aac /dev/vg0/lvm_root
+mount /dev/vg0/temp_xfs /tmp
+mount /dev/vg0/lvm_root /mnt
+xfsdump -J - /tmp | xfsrestore -J - /mnt
+umount /tmp
+umount /mnt
+lvm lvremove --config 'global {locking_type=1}' --yes -f /dev/vg0/temp_xfs
 }
 resize_local
