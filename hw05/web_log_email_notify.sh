f88703d52504d73e403943607adad0d4966c1405
Pavel Konotopov <kakoka@gmail.com>
2018-11-09 00:00:53 +0300

    centos version 1

--- hw05/web_log_email_notify.sh
+++ hw05/web_log_email_notify.sh
@@ -1,71 +1,72 @@
 #!/bin/bash
-
 ###############################
-#							  #
+#                              #
 # Web log email notify script #
-#							  #
+#                              #
 ###############################
-
-# sed
-# find
-
 # config section
+
 IFS=$' '
-pidfile=/var/run/wlen.pid
+PIDFILE=wlen.pid
 LOGDIR=logs
-LISTDIR=list
-recipient="kakoka@localhost"
-COUNT=7
+recipient="vagrant@localhost"
+COUNT=30
+
+# date setup
+hourago="85 hours ago"
+datt="`date +"%d/%b/%Y:%H"`"
+dacc="`date --date="$hourago" +"%d/%b/%Y:%H"`"
+derr="`date --date="$hourago" +"%Y/%m/%d-%H"`"
+errd="`echo $derr | sed 's/-/\ /'`"
 
 # functions
-
 send_email()
 {
-		(		
+        (
 cat - <<END
-Subject: hourly web server report 
+Subject: hourly web server report
 From: no-reply@localhost
 To: $recipient
 Content-Type: text/plain
+
+From the last hour there is some stats from web server
+
+Report from $dacc to $datt.
 
 We have some ip addresses:
 count ip-address
 ${IP_LIST[@]}
 
-We have some urls:
+and we have some urls:
 count url
 ${IP_ADDR[@]}
 
-We have some http codes:
+and we have some http codes:
 count code
 ${HTTP_STATUS[@]}
 
 We have some errors:
-${IP_ADDR[@]}
+${ERRORS[@]}
+
 END
 ) | /usr/sbin/sendmail $1
 }
 
 # here we start
 
-if [ ! -d $LISTDIR ]
+if [ -e $PIDFILE ]
 then
-	mkdir $LISTDIR
-fi
-
-if [ -e $LOCKFILE ]
-then
-	echo "isRunning!"
-	exit 1
+    echo "isRunning!"
+    exit 1
 else
-	echo "$$" > $LOCKFILE
-	trap 'rm -f $LOCKFILE; exit $?' INT TERM EXIT
-	IP_LIST+=(`awk -v dt=$(date --date="1 hour ago" +"%d/%m/%Y:%H") '$1==dt{print $1}' $LOGDIR/access.log | sort | uniq -c | sort -nr | head -$COUNT`)
-	IP_ADDR+=(`awk '{print $7}' $LOGDIR/access.log | sort | uniq -c | sort -nr | head -$COUNT`)
-	HTTP_STATUS+=(`awk '{print $9}' $LOGDIR/access.log | sort | uniq -c | sort -nr | head -$COUNT`)
-	ERRORS+=(`cat $LOGDIR/error.log | sort | uniq -c | head -$COUNT`)
-	send_email $recipient
-	rm -r $LOCKFILE
-	trap - INT TERM EXIT
-	echo "END"
+        echo "$$" > $PIDFILE
+        trap 'rm -f $PIDFILE; exit $?' INT TERM EXIT
+        IP_LIST+=(`cat $LOGDIR/access.log | grep "$dacc" | awk '{print $1}' | sort | uniq -c | sort -nr | head -$COUNT`)
+        IP_ADDR+=(`cat $LOGDIR/access.log | grep "$dacc" | awk '{print $7}' | sort | uniq -c | sort -nr | head -$COUNT`)
+        HTTP_STATUS+=(`cat $LOGDIR/access.log | grep "$dacc" | awk '{print $9}' | sort | uniq -c | sort -nr | head -$COUNT`)
+        ERRORS+=(`cat $LOGDIR/error.log | grep "$errd" | head -$COUNT`)
+        send_email $recipient
+        rm -r $PIDFILE
+        trap - INT TERM EXIT
+        echo "END"
 fi
